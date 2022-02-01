53a63fb6760d7c6eb664737a364dda8aff0878a7
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-02-01 07:31:10 +0100

    Remove comments

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
-// Copyright 2020 The Druid Authors.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-//! An example of sending commands from another thread.
-//! This is useful when you want to have some kind of
-//! generated content (like here), or some task that just
-//! takes a long time but don't want to block the main thread
-//! (waiting on an http request, some cpu intensive work etc.)
-
 // On Windows platform, don't show a console when opening the app.
 #![windows_subsystem = "windows"]
 
