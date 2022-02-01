82072a85f929bbcd3d818898ad8ec9f0b4ade676
Stefan <stefan_mack@hotmail.com>
2022-02-01 07:25:15 +0100

    Add spacing

--- src/main.rs
+++ src/main.rs
@@ -150,9 +150,11 @@ fn make_ui() -> impl Widget<AppData> {
     Flex::column()
         .with_flex_child(
             Flex::row()
-                .with_flex_child(top_left, 1.0)
-                .with_flex_child(top_right, 1.0),
-            1.0
+                .with_flex_child(top_left, 7.0)
+                .with_flex_spacer(0.1)
+                .with_flex_child(top_right, 7.0),
+            5.0
         )
-        .with_flex_child(bottom, 1.0)
+        .with_flex_spacer(0.1)
+        .with_flex_child(bottom, 5.0)
 }
