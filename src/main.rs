1211ddcab135e15115ca0d6698e13845d25efd38
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-02-01 08:05:27 +0100

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 // On Windows platform, don't show a console when opening the app.
 #![windows_subsystem = "windows"]
 
+use rand::Rng;
 use std::thread;
 use std::time::Duration;
-use rand::Rng;
 
-use druid::{widget::{Painter, Flex}, Data, Lens};
+use druid::{
+    widget::{Flex, Painter},
+    Data, Lens,
+};
 use druid::{AppLauncher, Color, RenderContext, Widget, WindowDesc};
 
 #[derive(Clone, Data, Lens)]
 struct AppData {
     top_left: Color,
     top_right: Color,
-    bottom: Color
+    bottom: Color,
 }
 
 pub fn main() {
@@ -34,7 +37,7 @@ pub fn main() {
     let data = AppData {
         top_left: Color::rgb8(0, 0, 0),
         top_right: Color::rgb8(0, 0, 0),
-        bottom: Color::rgb8(0, 0, 0)
+        bottom: Color::rgb8(0, 0, 0),
     };
 
     launcher
@@ -48,7 +51,7 @@ fn generate_colors(event_sink: druid::ExtEventSink) {
     // We take an `ExtEventSink` as an argument, we can use this event sink to send
     // commands to the main thread. Every time we generate a new colour we send it
     // to the main thread.
-    
+
     let mut rng = rand::thread_rng();
     let mut left_color = Color::BLACK;
     let mut right_color = Color::BLACK;
@@ -58,20 +61,14 @@ fn generate_colors(event_sink: druid::ExtEventSink) {
         let r = rng.gen_range(0, 2);
         let target;
         match r {
-            0 => {
-                target = Color::RED
-            }
-            1 => {
-                target = Color::BLUE
-            }
-            _ => {
-                target = Color::GREEN
-            }
+            0 => target = Color::RED,
+            1 => target = Color::BLUE,
+            _ => target = Color::GREEN,
         }
 
         reset(&mut left_color, &mut right_color, &mut bottom_color);
 
-        let r = rng.gen_range(0,3);
+        let r = rng.gen_range(0, 3);
         if r == 0 {
             left_color = target;
         } else if r == 1 {
@@ -114,7 +111,7 @@ fn reset(left: &mut Color, right: &mut Color, bottom: &mut Color) {
 fn make_ui() -> impl Widget<AppData> {
     let top_left = Painter::new(|ctx, data: &AppData, _env| {
         let rect = ctx.size().to_rounded_rect(5.0);
-        ctx.fill(rect, &data.top_left );
+        ctx.fill(rect, &data.top_left);
     });
 
     let top_right = Painter::new(|ctx, data: &AppData, _env| {
@@ -133,7 +130,7 @@ fn make_ui() -> impl Widget<AppData> {
                 .with_flex_child(top_left, 7.0)
                 .with_flex_spacer(0.1)
                 .with_flex_child(top_right, 7.0),
-            5.0
+            5.0,
         )
         .with_flex_spacer(0.1)
         .with_flex_child(bottom, 5.0)
